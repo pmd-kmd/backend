ce758583da8145681302ec70aafb9b4d6ced9d4e
pmd-kmd <PMD@kmd.dk>
2025-10-23 10:04:05 +0530

    Dummy springboot

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
